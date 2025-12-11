e167c5bc841afe095170e15938d634f5667d146d
Alejandro Quintanar <alejandro@quihel.net>
2025-12-11 10:36:30 +0100

    Update term39 to v0.20.0

--- Formula/term39.rb
+++ Formula/term39.rb
@@ -1,30 +1,30 @@
 class Term39 < Formula
   desc "Modern, retro-styled terminal multiplexer with a classic MS-DOS aesthetic"
   homepage "https://github.com/alejandroqh/term39"
-  version "0.19.5"
+  version "0.20.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/alejandroqh/term39/releases/download/v0.19.5/term39-0.19.5-macos-64bit-arm-binary.tar.gz"
-      sha256 "b407dd55e49c37484ee2d8adc40c04ce46a53c1f28cd428cf7da64606919338c"
+      url "https://github.com/alejandroqh/term39/releases/download/v0.20.0/term39-0.20.0-macos-64bit-arm-binary.tar.gz"
+      sha256 "e93f9f4f003919c14b60d6c0f64468409d1d550b89a87de0e66b8e5148ddaf16"
     end
 
     on_intel do
-      url "https://github.com/alejandroqh/term39/releases/download/v0.19.5/term39-0.19.5-macos-64bit-x86-binary.tar.gz"
-      sha256 "a912f0f0ee9621e4321ffd1ad0c6761f65ba031dfda95020a3abac27724edf34"
+      url "https://github.com/alejandroqh/term39/releases/download/v0.20.0/term39-0.20.0-macos-64bit-x86-binary.tar.gz"
+      sha256 "16c36dbd2c6293a3b26cfcbdd5b6fd422d6329c35a5f76029301332264892d37"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/alejandroqh/term39/releases/download/v0.19.5/term39-0.19.5-linux-64bit-arm-binary.tar.gz"
-      sha256 "7ef96825b5930ebf2020a817e4e9c39f2560c992a6bcf1bcfe80b8d098c99361"
+      url "https://github.com/alejandroqh/term39/releases/download/v0.20.0/term39-0.20.0-linux-64bit-arm-binary.tar.gz"
+      sha256 "f3d7d5a98c7a90d4abd5ef34c1079dfda74c28955593d74c117d5a4573cf47aa"
     end
 
     on_intel do
-      url "https://github.com/alejandroqh/term39/releases/download/v0.19.5/term39-0.19.5-linux-64bit-x86-binary.tar.gz"
-      sha256 "25a70b6df36a5f4dba6d091a4057e8f74da4cb52fa2ad8065504a1355b19dff0"
+      url "https://github.com/alejandroqh/term39/releases/download/v0.20.0/term39-0.20.0-linux-64bit-x86-binary.tar.gz"
+      sha256 "cad0018dd4ba5c346dbd9c69640b9a9fa0603d3854f2446e2d072d9532f44802"
     end
   end
 
